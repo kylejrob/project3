376a1eb591c76872e26e72517a362431fe62498e
Cleve716 <cleve716@msn.com>
2019-08-16 14:30:38 -0600

    video code for new branch

---
+++ client/src/components/Pages/home/components/video/video.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+export default class VideoGrid extends React.Component {
+
+    render() {
+        return (
+            <>
+                <h1>VIDEOS</h1>
+
+                <p>This is where the video grid comes from... pushing into highScoresTable from video.js
+                    ---------- blah...blah... blah...blah...blah...blah... blah...blah...blah...blah... blah...blah...
+                </p>
+            </>
+        )
+    }
+};
