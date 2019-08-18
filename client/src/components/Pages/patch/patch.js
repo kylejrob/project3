ab7113d516192ac5a9df741107abf489848eacab
Kyle <kylejrob@gmail.com>
2019-08-18 13:17:14 -0600

    Patch page

--- client/src/components/Pages/patch/patch.js
+++ client/src/components/Pages/patch/patch.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { Transition } from "react-transition-group";
 import { TweenLite } from "gsap/all";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 
 
@@ -22,15 +31,103 @@ export const Patch = props => <Transition
 
 	
 >	
-<div>
-        <div className="patch">
+<body>
+<Grid container spacing={1}>
+        <Grid item xs={12}>
                         <ul>
                             <li>Ability to change ship size</li>
                             <li>Ability to change bullet size</li>
                             <li>Added shop to purchase different ships</li>
                         </ul>
-                        </div>
+        </Grid>
+		<Grid item xs={6}>
+			<h2>Mike</h2>
+			<Card>
+				<CardContent>
+					<Typography color="textSecondary">Program Developer</Typography>
+					<Typography variant="h5" component="h2">
+						React
+					</Typography>
+					<CardActions>
+						<Button size="small" href="https://cleve716.github.io/detailed-portfolio/">Portfolio</Button>
+					</CardActions>
+				</CardContent>
+			</Card>
+		</Grid>
+
+		<Grid item xs={6}>
+			<h2>Stet</h2>
+			<Card>
+				<CardContent>
+					<Typography color="textSecondary">Program Developer</Typography>
+					<Typography variant="h5" component="h2">
+						React
+					</Typography>
+					<CardActions>
+						<Button size="small" href="https://solaye.github.io/Updated-Portfolio/">Portfolio</Button>
+					</CardActions>
+				</CardContent>
+			</Card>
+		</Grid>
+
+		<Grid item xs={6}>
+			<h2>Kyle</h2>
+			<Card>
+				<CardContent>
+					<Typography color="textSecondary">Program Developer</Typography>
+					<Typography variant="h5" component="h2">
+						React
+					</Typography>
+					<CardActions>
+						<Button size="small" href="https://kylejrob.github.io/designPortfolio/">Portfolio</Button>
+					</CardActions>
+				</CardContent>
+			</Card>
+		</Grid>
+
+		<Grid item xs={6}>
+			<h2>Jeff</h2>
+			<Card>
+				<CardContent>
+					<Typography color="textSecondary">Program Developer</Typography>
+					<Typography variant="h5" component="h2">
+						React
+					</Typography>
+					<CardActions>
+						<Button size="small" href="https://fleerless.github.io/Responsive-Portfolio/">Portfolio</Button>
+					</CardActions>
+				</CardContent>
+			</Card>
+		</Grid>
+		
+
+		{/* <div className={paper>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
+    </div> */}
 
 
-	</div>
+	</Grid>
+	</body>
 </Transition>;
