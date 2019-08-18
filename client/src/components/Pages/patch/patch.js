b244fc675979ff0bfb78d68d7ac71ec5858f6829
Kyle <kylejrob@gmail.com>
2019-08-18 14:25:25 -0600

    patch page 3

--- client/src/components/Pages/patch/patch.js
+++ client/src/components/Pages/patch/patch.js
@@ -34,68 +34,106 @@ export const Patch = props => <Transition
 <body>
 <Grid container spacing={1}>
         <Grid item xs={12}>
+			<div className="patchNotes">
                         <ul>
                             <li>Ability to change ship size</li>
                             <li>Ability to change bullet size</li>
                             <li>Added shop to purchase different ships</li>
                         </ul>
+						</div>
         </Grid>
 		<Grid item xs={6}>
+		<div className="teamCards">
 			<Card>
 				<CardContent>
+					<div className="cardNames">
 					<Typography variant="h5" component="h2">Mike Santarelli</Typography>
+					</div>
 					<Typography color="textSecondary">
 						React Developer
 					</Typography>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat nisl pretium fusce id velit ut tortor pretium. Scelerisque in dictum non consectetur a.</p>
+					<Typography>
+					</Typography>
 					<CardActions>
+					<div className="portfolioButton">
 						<Button size="small" href="https://cleve716.github.io/detailed-portfolio/">Portfolio</Button>
+					</div>
 					</CardActions>
 				</CardContent>
 			</Card>
+			</div>
 		</Grid>
 
 		<Grid item xs={6}>
+		<div className="teamCards">
 			<Card>
 				<CardContent>
+				<div className="cardNames">
 				<Typography variant="h5" component="h2">Stet Olaye</Typography>
+				</div>
 					<Typography color="textSecondary">
 						React Developer
 					</Typography>
+					<Typography>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium vulputate sapien nec sagittis aliquam. Mi quis hendrerit dolor magna eget est lorem ipsum.</p>
+					</Typography>
 					<CardActions>
+					<div className="portfolioButton">
 						<Button size="small" href="https://solaye.github.io/Updated-Portfolio/">Portfolio</Button>
+					</div>
 					</CardActions>
 				</CardContent>
 			</Card>
+			</div>
 		</Grid>
 
 		<Grid item xs={6}>
+		<div className="teamCards">
 			<Card>
 				<CardContent>
+					<div className="cardNames">
 					<Typography variant="h5" component="h2">Kyle Robinson</Typography>
+					</div>
 					<Typography color="textSecondary">
 						React Developer
 					</Typography>
+					<Typography>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium vulputate sapien nec sagittis aliquam. Mi quis hendrerit dolor magna eget est lorem ipsum.</p>
+					</Typography>
 					<CardActions>
+						<div className="portfolioButton">
 						<Button size="small" href="https://kylejrob.github.io/designPortfolio/">Portfolio</Button>
+						</div>
 					</CardActions>
 				</CardContent>
 			</Card>
-		</Grid>
-
-		<Grid item xs={6}>
-			<Card>
-				<CardContent>
-					<Typography variant="h5" component="h2">Jeff Fleer</Typography>
-					<Typography color="textSecondary">
-						React Developer
-					</Typography>
-					<CardActions>
-						<Button size="small" href="https://fleerless.github.io/Responsive-Portfolio/">Portfolio</Button>
-					</CardActions>
-				</CardContent>
-			</Card>
+			</div>
 		</Grid>
 		
+		<Grid item xs={6}>
+		<div className="teamCards">
+			<Card>
+				<CardContent>
+				<div className="cardNames">
+					<Typography variant="h5" component="h2">Jeff Fleer</Typography>
+					</div>
+					<Typography color="textSecondary">
+						React Developer
+					</Typography>
+					<Typography>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium vulputate sapien nec sagittis aliquam. Mi quis hendrerit dolor magna eget est lorem ipsum.</p>
+					</Typography>
+					<CardActions>
+					<div className="portfolioButton">
+						<Button size="small" href="https://fleerless.github.io/Responsive-Portfolio/">Portfolio</Button>
+					</div>
+					</CardActions>
+				</CardContent>
+			</Card>
+			</div>
+		</Grid>
+
 
 		{/* <div className={paper>
       <Grid container spacing={3}>
