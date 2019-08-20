0ea1fd3dbf6a2133052c3c68b29d9162d93b2619
Kyle <kylejrob@gmail.com>
2019-08-19 21:22:30 -0600

    video grid

--- client/src/components/Pages/scores/Video.js
+++ client/src/components/Pages/scores/Video.js
@@ -1,27 +1,43 @@
-import React from "react"
+import React from "react";
 import Grid from '@material-ui/core/Grid';
+import { Typography } from "@material-ui/core";
+
+
 
 export default class VideoGrid extends React.Component {
 
     render() {
         return (
-            <>
-                <div className = "videoBody"><h1>VIDEOS</h1></div>
-                <Grid container spacing={4}>
-                    <Grid item xs={12}>
-                        video1
-                    </Grid>
-                    <Grid item xs={12}>
-                        video2
-                    </Grid>
-                    <Grid item xs={12}>
-                        video3
-                    </Grid>
-                    <Grid item xs={12}>
-                        video4
-                    </Grid>
-                  
+            <>  
+                <Grid container spacing={12}>
+                    <Grid>
+                        
+                   <input className="searchBarVideo" type="text" placeholder="Search.."></input>
+                        
+                       </Grid>
                 </Grid>
+
+                    <Grid container spacing={12}>
+                            <Grid direction="row" item xs={8}>
+                            <Typography><iframe width="600" height="400" src="https://www.youtube.com/embed/bU1QPtOZQZU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Typography>
+                            </Grid>
+                            <Grid direction="row" item xs={4}>
+                                <Grid>
+                                <iframe width="300" height="200" src="https://www.youtube.com/embed/Hs2LY5NYL0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </Grid>
+                                <Grid>
+                                <iframe width="300" height="200" src="https://www.youtube.com/embed/Hs2LY5NYL0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </Grid>
+                                <Grid>
+                                <iframe width="300" height="200" src="https://www.youtube.com/embed/Hs2LY5NYL0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </Grid>
+                                 <Grid>
+                                 <iframe width="300" height="200" src="https://www.youtube.com/embed/Hs2LY5NYL0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </Grid>
+                            </Grid>
+                    </Grid>
+                    
+                
             </>
         )
     }
